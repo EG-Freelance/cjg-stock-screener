d68ece95c96ec040bd5540aee72296beb3df9dad
Erik <flyers_rock_87@hotmail.com>
2017-01-25 22:52:28 +0000

    migration hotfix

--- db/migrate/20170124203255_separate_descriptions_for_stock.rb
+++ db/migrate/20170124203255_separate_descriptions_for_stock.rb
@@ -1,6 +1,5 @@
 class SeparateDescriptionsForStock < ActiveRecord::Migration
   def change
-    remove_column :stocks, :description
     add_column :stocks, :si_description, :string
     add_column :stocks, :pi_description, :string
   end

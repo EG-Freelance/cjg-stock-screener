0b34e433da07184dbf32dd3ef0e312106bb9a474
Erik <flyers_rock_87@hotmail.com>
2017-02-21 19:33:48 +0000

    test sorting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -113,7 +113,7 @@ class PagesController < ApplicationController
         rq_rank,
         dt2_rank,
         prev_ed.empty? ? "N/A" : (Date.today - prev_ed.last.date).to_i,
-        next_ed.empty? ? "N/A" : (next_ed.last.date - Date.today).to_i
+        next_ed.empty? ? "N/A" : (next_ed.last.date - Date.today).to_i == 0 ? "Today" : (next_ed.last.date - Date.today).to_i
       ]
     end
     # calculate programmatic action (si[4]), total score percentile (si[7]) and dist > 7 or 8 (si[8]) after initial setup
@@ -255,7 +255,7 @@ class PagesController < ApplicationController
         rq_rank,
         dt2_rank,
         prev_ed.empty? ? "N/A" : (Date.today - prev_ed.last.date).to_i,
-        next_ed.empty? ? "N/A" : (next_ed.last.date - Date.today).to_i
+        next_ed.empty? ? "N/A" : (next_ed.last.date - Date.today).to_i == 0 ? "Today" : (next_ed.last.date - Date.today).to_i
       ]
     end
     # calculate total score percentile (si[7]) and dist > 7 or 8 (si[8]) after initial setup

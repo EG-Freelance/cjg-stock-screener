a9e24b908f9ada3934251e8a0706c320f81e2235
Erik <flyers_rock_87@hotmail.com>
2017-02-23 19:55:02 +0000

    add multi-phrase filtering and case insensitivity

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,11 +1,31 @@
-function filterText(){
-	$('tr').show();
-	var searchText = $('#search-text').val();
+// script to allow for case-insensitive filtering
+$.extend($.expr[':'], {
+  'containsi': function(elem, i, match, array)
+  {
+    return (elem.textContent || elem.innerText || '').toLowerCase()
+    .indexOf((match[3] || "").toLowerCase()) >= 0;
+  }
+});
+
+// function filterText(){
+// 	$('tr').show();
+// 	var searchText = $('#search-text').val();
 	
-	var hide = $('tr:not(:contains("Exchange")):not(:contains("'+searchText+'"))');
+// 	var hide = $('tr:not(:contains("Exchange")):not(:containsi("'+searchText+'"))');
+// 	$.each(hide, function(){ $(this).hide() });
+// }
+
+function filterMultiText(){
+	$('tr').show();
+	var searchText2 = $('#search-text-2').val().replace(/\s\|\|\s/g,"||");
+	var searchText2String = searchText2.split("||").map(function(e){ return ':not(:containsi("'+e+'"))' }).join("")
+	
+	var hide = $('tr:not(:contains("Exchange"))'+searchText2String);
 	$.each(hide, function(){ $(this).hide() });
 }
 
+
 $(document).ready(function(){
-	$('#search-text').keyup(filterText);
+	//$('#search-text').keyup(filterText);
+	$('#search-text-2').keyup(filterMultiText);
 });

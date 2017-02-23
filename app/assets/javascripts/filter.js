be8c6cd8eeebb2067ffe4b05dd19ecc24dd3afbf
Erik <flyers_rock_87@hotmail.com>
2017-02-23 01:19:40 +0000

    remove superfluous code

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,12 +1,10 @@
 function filterText(){
-	$('tr').show()
+	$('tr').show();
 	var searchText = $('#search-text').val();
 	
 	var hide = $('tr:not(:contains("Exchange")):not(:contains("'+searchText+'"))');
 	$.each(hide, function(){ $(this).hide() });
 }
-
-$('#search-text').keyup(filterText);
 
 $(document).ready(function(){
 	$('#search-text').keyup(filterText);

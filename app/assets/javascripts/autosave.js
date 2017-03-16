761c7fd43d848467553cff314a0285b7566fd9fa
Erik <flyers_rock_87@hotmail.com>
2017-03-16 13:56:52 +0000

    save action when any other field is clicked

--- app/assets/javascripts/autosave.js
+++ app/assets/javascripts/autosave.js
@@ -2,19 +2,24 @@ function updateActions() {
 	var oldText;
 	var newText;
 	var submitId;
+	var el;
 	$('.action-field').focusin(function(){
 		oldText = $(this).val();
+		el = $(this);
+		console.log(el);
 		submitId = $(this).attr('id').replace('action', 'submit');
+		
+		// set function for exit of this field
+		el.focusout(function(){
+			newText = $(this).val();
+			if(newText == oldText){
+				return false
+			}else{
+				$('#'+submitId).trigger('click');
+			}
+		});
 	});
 	
-	$('.action-field').focusout(function(){
-		newText = $(this).val();
-		if(newText == oldText){
-			return false
-		}else{
-			$('#'+submitId).trigger('click');
-		}
-	});
 }
 
 $(document).ready(updateActions);

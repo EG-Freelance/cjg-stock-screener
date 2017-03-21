38969ac6ebe3e27103300ec1c3e12353b101f4cd
Erik <flyers_rock_87@hotmail.com>
2017-03-21 16:23:35 +0000

    update filters to include hiding holds and/or N/As

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -7,19 +7,50 @@ $.extend($.expr[':'], {
   }
 });
 
+function unhideFiltered(){
+	$('tr.hide:not(.filter-hide, .na-hide, .hold-hide)').removeClass('hide');
+}
+
 function filterMultiText(){
-	$('tr').removeClass('hide');
+	$('tr.filter-hide').removeClass('filter-hide');
+	unhideFiltered();
 	var searchText2 = $('#search-text-2').val().replace(/\s\|\|\s/g,"||");
 	var searchText2String = searchText2.split("||").map(function(e){ return ':not(:containsi("'+e+'"))' }).join("")
 	
-	var hide = $('tr:not(:contains("Exchange"))'+searchText2String);
-	$.each(hide, function(){ $(this).addClass('hide') });
+	var hide = $('tr:not(.tablesorter-headerRow)'+searchText2String);
+	$.each(hide, function(){ $(this).addClass('hide filter-hide') });
 	$('tr:not(.hide):even').addClass('even').removeClass('odd');
 	$('tr:not(.hide):odd').addClass('odd').removeClass('even');
 }
 
+function autoFilterNa(){
+	$('tr.na-hide').removeClass('na-hide');
+	unhideFiltered();
+	if($('#hide-na').prop('checked') == true){
+		var filterText = "(n/a)"
+	
+		var hide = $('tr:not(.tablesorter-headerRow) td:nth-child(7):contains('+filterText+')').parent();
+		$.each(hide, function(){ $(this).addClass('hide na-hide') });
+		$('tr:not(.hide):even').addClass('even').removeClass('odd');
+		$('tr:not(.hide):odd').addClass('odd').removeClass('even');
+	}
+}
+
+function autoFilterHolds(){
+	$('tr.hold-hide').removeClass('hold-hide');
+	unhideFiltered();
+	if($('#hide-holds').prop('checked') == true){
+		var filterText = "HOLD"
+	
+		var hide = $('tr:not(.tablesorter-headerRow) td:nth-child(7):contains('+filterText+')').parent();
+		$.each(hide, function(){ $(this).addClass('hide hold-hide') });
+		$('tr:not(.hide):even').addClass('even').removeClass('odd');
+		$('tr:not(.hide):odd').addClass('odd').removeClass('even');
+	}
+}
 
 $(document).ready(function(){
-	//$('#search-text').keyup(filterText);
 	$('#search-text-2').keyup(filterMultiText);
+	$('#hide-na').change(autoFilterNa);
+	$('#hide-holds').change(autoFilterHolds);
 });

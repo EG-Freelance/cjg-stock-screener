c1b67523d77d44d08e8ff71ed367d8a218c6116d
Erik <flyers_rock_87@hotmail.com>
2017-03-22 13:23:41 +0000

    add initial sort back in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -42,8 +42,8 @@ class PagesController < ApplicationController
     si_pool_lg = display_items.where(classification: "large")
     si_pool_sm = display_items.where(classification: "small")
     po_pool = display_items.where(classification: "fallen out")
-    @si_lg = si_pool_lg.map { |si| [si.symbol, si.exchange, si.company, si.in_pf, si.rec_action, si.action, si.total_score, si.total_score_pct, si.dist_status, si.mkt_cap, si.nsi_score, si.ra_score, si.noas_score, si.ag_score, si.aita_score, si.l52wp_score, si.pp_score, si.rq_score, si.dt2_score, si.prev_ed, si.next_ed, si.lq_revenue, si.stock.portfolio_items, lg_purch.include?(si) ? (si.mkt_cap / lg_cap_base.to_f ) * 2800000 : 0] }
-    @si_sm = si_pool_sm.map { |si| [si.symbol, si.exchange, si.company, si.in_pf, si.rec_action, si.action, si.total_score, si.total_score_pct, si.dist_status, si.mkt_cap, si.nsi_score, si.ra_score, si.noas_score, si.ag_score, si.aita_score, si.l52wp_score, si.pp_score, si.rq_score, si.dt2_score, si.prev_ed, si.next_ed, si.lq_revenue, si.stock.portfolio_items, sm_purch.include?(si) ? (si.mkt_cap / sm_cap_base.to_f ) * 2800000 : 0] }
+    @si_lg = si_pool_lg.map { |si| [si.symbol, si.exchange, si.company, si.in_pf, si.rec_action, si.action, si.total_score, si.total_score_pct, si.dist_status, si.mkt_cap, si.nsi_score, si.ra_score, si.noas_score, si.ag_score, si.aita_score, si.l52wp_score, si.pp_score, si.rq_score, si.dt2_score, si.prev_ed, si.next_ed, si.lq_revenue, si.stock.portfolio_items, lg_purch.include?(si) ? (si.mkt_cap / lg_cap_base.to_f ) * 2800000 : 0] }.sort_by { |si| si[7] }.reverse!
+    @si_sm = si_pool_sm.map { |si| [si.symbol, si.exchange, si.company, si.in_pf, si.rec_action, si.action, si.total_score, si.total_score_pct, si.dist_status, si.mkt_cap, si.nsi_score, si.ra_score, si.noas_score, si.ag_score, si.aita_score, si.l52wp_score, si.pp_score, si.rq_score, si.dt2_score, si.prev_ed, si.next_ed, si.lq_revenue, si.stock.portfolio_items, sm_purch.include?(si) ? (si.mkt_cap / sm_cap_base.to_f ) * 2800000 : 0] }.sort_by { |si| si[7] }.reverse!
     
     # for development, just replicate lg pool (so there are multiple tabs of data0)
     if Rails.env == "development"

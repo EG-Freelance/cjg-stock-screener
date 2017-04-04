c25a40d75b0804e45434e3f7fa209ede2a4ecf53
Erik <flyers_rock_87@hotmail.com>
2017-04-04 17:12:41 +0000

    hotfix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404143339) do
+ActiveRecord::Schema.define(version: 20170404171058) do
 
   create_table "actions", force: :cascade do |t|
     t.integer  "stock_id"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20170404143339) do
 
   create_table "cashes", force: :cascade do |t|
     t.decimal  "amount"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "data_sets", force: :cascade do |t|

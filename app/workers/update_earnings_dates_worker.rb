00b61756c6505ed4cfac8507a9264620ca881f30
Erik <flyers_rock_87@hotmail.com>
2017-04-29 21:10:05 +0000

    fix earnings scraper so that it pulls every page of earnings

--- app/workers/update_earnings_dates_worker.rb
+++ app/workers/update_earnings_dates_worker.rb
@@ -19,43 +19,54 @@ class UpdateEarningsDatesWorker
     base = "http://finance.yahoo.com/calendar/earnings?day="
     
     date_string = date.strftime("%Y-%m-%d")
+    
+    url = base + date_string
 
     begin
-      response = agent.get(base + date_string)
+      response = agent.get(url)
+      results = response.search('span').find { |s| s.text.match(/d*\sresults/) }.text.match(/\d*\-\d*\sof\s(\d*)\sresults/)[1].to_i
     rescue
       puts "No earnings or date error for #{date}"
       return false
     end
     
-    # financial calendar table
-    table = response.at "#fin-cal-table"
+    pages = (results/100.0).ceil
     
-    # rows of usable data
-    rows = table.search('tr')[1..-1]
-    
-    # array of usable data arrays
-    data = rows.map { |r| r.search('td').map { |c| c.text } }
-    
-    # don't use symbols with decimal points (foreign exchanges)
-    data.delete_if { |d| d[0].match(/\./) }
-    
-    # remove data for stocks that aren't in the system
-    data.delete_if { |d| !sym_array.include?(d[0]) }
-    
-    # remove earnings dates that no longer appear on this page
-    ed_delete_array = ed_today_array.delete_if { |ed| data.map { |d| d[0] }.include?(ed.stock.symbol) }
-    ed_delete_array.each { |ed| ed.destroy }
-    
-    data.each do |d|
-      # select the stock in the system
-      stock = stocks.find_by(symbol: d[0])
-      # save the current date as an entry with the provided time
-      stock.earnings_dates.where(date: date, time: d[5]).first_or_create   
-      
-      # delete unneeded old earnings dates (all except most recent and upcoming) to maintain DB size
-      if stock.earnings_dates.count >= 3
-        stock.earnings_dates[0..-3].each { |e| e.destroy }
+    pages.times do |i|
+      unless i == 0
+        paginated_url = url + "&offset=#{i * 100}&size=100"
+        response = agent.get(paginated_url)
       end
-    end # end data
-  end
+      # financial calendar table
+      table = response.at "#fin-cal-table"
+      
+      # rows of usable data
+      rows = table.search('tr')[1..-1]
+      
+      # array of usable data arrays
+      data = rows.map { |r| r.search('td').map { |c| c.text } }
+      
+      # don't use symbols with decimal points (foreign exchanges)
+      data.delete_if { |d| d[0].match(/\./) }
+      
+      # remove data for stocks that aren't in the system
+      data.delete_if { |d| !sym_array.include?(d[0]) }
+      
+      # remove earnings dates that no longer appear on this page
+      ed_delete_array = ed_today_array.delete_if { |ed| data.map { |d| d[0] }.include?(ed.stock.symbol) }
+      ed_delete_array.each { |ed| ed.destroy }
+      
+      data.each do |d|
+        # select the stock in the system
+        stock = stocks.find_by(symbol: d[0])
+        # save the current date as an entry with the provided time
+        stock.earnings_dates.where(date: date, time: d[5]).first_or_create   
+        
+        # delete unneeded old earnings dates (all except most recent and upcoming) to maintain DB size
+        if stock.earnings_dates.count >= 3
+          stock.earnings_dates[0..-3].each { |e| e.destroy }
+        end
+      end # end data
+    end # end pages.times loop
+  end # end process
 end

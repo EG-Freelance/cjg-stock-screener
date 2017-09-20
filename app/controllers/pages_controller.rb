321a76d325270c6c4026e81b82dc22dfb73be21e
Erik <flyers_rock_87@hotmail.com>
2017-09-20 15:01:43 +0000

    update transactions spreadsheet formatting

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -439,6 +439,7 @@ class PagesController < ApplicationController
     ####### ROW/CELL FORMATS ########
     header_format = Spreadsheet::Format.new :weight => :bold, :border => :thin, :horizontal_align => :center, :pattern_fg_color => :lime, :pattern => 1, :size => 9, :text_wrap => true, :vertical_align => :top
     default_format = Spreadsheet::Format.new :border => :thin, :horizontal_align => :center, :size => 9, :text_wrap => true, :vertical_align => :top
+    date_format = Spreadsheet::Format.new :number_format => 'YYYY-MM-DD'
 
     # set headers
     page.row(0).push "Position Information", "", "", "", "", "", "", "", "", "", "", "Acquisition Information", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "Close/Current Information", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""
@@ -449,7 +450,9 @@ class PagesController < ApplicationController
     
     transaction_items = TransactionItem.all
     transaction_items.each_with_index do |ti, i|
-      page.row(i+2).push ti.symbol, ti.exchange, ti.company, ti.position, ti.pos_type, ti.op_type, ti.op_strike, ti.op_expiration, ti.quantity, ti.active, "", ti.date_acq, ti.paid, ti.rec_action_o, ti.total_score_o, ti.total_score_pct_o, ti.nsi_score_o, ti.ra_score_o, ti.noas_score_o, ti.ag_score_o, ti.aita_score_o, ti.l52wp_score_o, ti.pp_score_o, ti.rq_score_o, ti.dt2_score_o, ti.prev_ed_o, ti.next_ed_o, ti.mkt_cap_o, ti.lq_revenue_o, "", ti.date_sold, ti.last, ti.rec_action_c, ti.total_score_c, ti.total_score_pct_c, ti.nsi_score_c, ti.ra_score_c, ti.noas_score_c, ti.ag_score_c, ti.aita_score_c, ti.l52wp_score_c, ti.pp_score_c, ti.rq_score_c, ti.dt2_score_c, ti.prev_ed_c, ti.next_ed_c, ti.mkt_cap_c, ti.lq_revenue_c
+      page.row(i+2).push ti.symbol, ti.exchange, ti.company, ti.position, ti.pos_type, ti.op_type, ti.op_strike, ti.op_expiration, ti.quantity, ti.active, "", ti.date_acq, ti.paid, ti.rec_action_o, ti.total_score_o, ti.total_score_pct_o, ti.nsi_score_o, ti.ra_score_o, ti.noas_score_o, ti.ag_score_o, ti.aita_score_o, ti.l52wp_score_o, ti.pp_score_o, ti.rq_score_o, ti.dt2_score_o, ti.prev_ed_o, ti.next_ed_o, ti.mkt_cap_o, ti.lq_revenue_o, "", ti.active ? "N/A" : ti.date_sold.strftime("%Y-%m-%d"), ti.last, ti.rec_action_c, ti.total_score_c, ti.total_score_pct_c, ti.nsi_score_c, ti.ra_score_c, ti.noas_score_c, ti.ag_score_c, ti.aita_score_c, ti.l52wp_score_c, ti.pp_score_c, ti.rq_score_c, ti.dt2_score_c, ti.prev_ed_c, ti.next_ed_c, ti.mkt_cap_c, ti.lq_revenue_c
+      page.row(i+2).set_format(11, date_format)
+      page.row(i+2).set_format(30, date_format) if !ti.active
     end
     
     summary = StringIO.new

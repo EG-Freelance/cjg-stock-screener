c7b8eb743a447a701d83135c02289993bc3a6b6a
Erik <flyers_rock_87@hotmail.com>
2018-04-18 22:49:55 +0000

    attachment bugfix

--- app/mailers/update_mailer.rb
+++ app/mailers/update_mailer.rb
@@ -5,7 +5,7 @@ class UpdateMailer < ApplicationMailer
 		data = DisplayItem.create_xls
 		spreadsheet_file = StringIO.new
 		data.write(spreadsheet_file)
-		attachments["Screen Update #{Date.today.strftime('%F')}.xls"] = spreadsheet_file.read
+		attachments["Screen Update #{Date.today.strftime('%F')}.xls"] = File.open(spreadsheet_file, 'rb'){ |f| f.read }
 		
 		mail(to: "erik.w.gibbons@gmail.com", subject: "Screen Update")
 	end

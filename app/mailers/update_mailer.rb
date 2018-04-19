9f88de1623a4850ebca795e58f484afbff72e830
Erik <flyers_rock_87@hotmail.com>
2018-04-19 03:34:36 +0000

    bugfix in attachment

--- app/mailers/update_mailer.rb
+++ app/mailers/update_mailer.rb
@@ -1,12 +1,16 @@
 class UpdateMailer < ApplicationMailer
-	default from: 'stock.screener.updates@gmail.com'
-	
-	def update_email
-		data = DisplayItem.create_xls
-		spreadsheet_file = StringIO.new
-		data.write(spreadsheet_file)
-		attachments["Screen Update #{Date.today.strftime('%F')}.xls"] = File.open(spreadsheet_file, 'rb'){ |f| f.read }
-		
-		mail(to: "erik.w.gibbons@gmail.com", subject: "Screen Update")
-	end
+  default from: 'stock.screener.updates@gmail.com'
+  
+  def update_email
+    data = DisplayItem.create_xls
+    spreadsheet_file = StringIO.new
+    data.write(spreadsheet_file)
+    file = Tempfile.new(['temp','.xls'])
+    file.binmode
+    file.write spreadsheet_file.read
+    
+    attachments["Screen Update #{Date.today.strftime('%F')}.xls"] = File.open(file, 'rb'){ |f| f.read }
+    
+    mail(to: "erik.w.gibbons@gmail.com", subject: "Screen Update")
+  end
 end

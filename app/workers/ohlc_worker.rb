e30df4b40d479f95d9de4f409f448407e3a624cc
Erik <flyers_rock_87@hotmail.com>
2018-05-24 19:47:18 +0000

    write data to spreadsheet piecewise to not take up extra memory of housing entire symbol/date hash

--- app/workers/ohlc_worker.rb
+++ app/workers/ohlc_worker.rb
@@ -4,49 +4,6 @@ class OhlcWorker
   sidekiq_options queue: 'high', unique: :until_executed
   
   def perform(s_array, email)
-    # set agent
-    agent = Mechanize.new
-    
-    # remove unnecessary header 
-    s_array = s_array[1..-1]
-    
-    # separate rows with populated data [ [empty_data], [populated_data] ]
-    part = s_array.partition { |s| s[2].nil? }
-    
-    # get unique stock symbols (except where data are already provided)
-    uniq_sym = part[0].map { |s| s[0] }.uniq
-    
-    # create OHLC data container
-    ohlc_hash = {}
-    
-    puts "Populating data..."
-    # populate data
-    uniq_sym.each_with_index do |s,i|
-      if i % 250 == 0
-        agent.get("http://cjg-stock-screener.herokuapp.com/ohlc")
-      end
-      retries = 0
-      begin
-        response = agent.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=#{s}&apikey=#{ENV['AA_KEY']}")
-        resp = JSON.parse(response.body)
-        if !resp["Information"].nil?
-          raise
-        end
-        ohlc_hash[s] = resp
-        puts "Succeeded with #{s}"
-        sleep(1)
-      rescue
-        if retries < 100
-          puts "Failed #{s}, try ##{retries + 1}"
-          retries += 1
-          sleep(4)
-          retry
-        else
-          next
-        end
-      end
-    end
-
     # create output workbook
     output = Spreadsheet::Workbook.new
     
@@ -62,24 +19,85 @@ class OhlcWorker
       page.row(0).set_format(i, header_format)
     end
     
-    puts "Populating output spreadsheet..."
-    # populate output spreadsheet
-    s_array.each_with_index do |s,i|
-      if s[2].nil?
-        # skip if problem pulling data
-        if !ohlc_hash[s[0]]["Information"].nil?
-          page.row(i+1).push s[0], s[1], "API called too fast", "API called too fast", "API called too fast", "API called too fast"
+    # set agent
+    agent = Mechanize.new
+    
+    # remove unnecessary header 
+    s_array = s_array[1..-1]
+    
+    # separate rows with populated data [ [empty_data], [populated_data] ]
+    # part = s_array.partition { |s| s[2].nil? }
+    
+    # get unique stock symbols (except where data are already provided)
+    # uniq_sym = part[0].map { |s| s[0] }.uniq
+    
+    uniq_sym = s_array.map { |s| s[0] }.uniq
+    
+    # create OHLC data container
+    # ohlc_hash = {}
+    
+    # loop params
+    r = 1
+    loop_start = Time.now - 1.second
+    loop_end = Time.now - 1.second
+    
+    puts "Populating data and writing to output..."
+    # populate data
+    uniq_sym.each_with_index do |s,i|
+      if i % 250 == 0
+        agent.get("http://cjg-stock-screener.herokuapp.com/ohlc")
+      end
+      retries = 0
+      begin
+        # evaluate to make sure at least a second has passed since the last response request
+        loop_end = Time.now
+        loop_diff = loop_end - loop_start
+        if loop_diff > 1
+          sleep(1-loop_diff)
         end
-        if ohlc_hash[s[0]]['Time Series (Daily)'][s[1]].nil?
-          page.row(i+1).push s[0], s[1], "N/A (no data)", "N/A (no data)", "N/A (no data)", "N/A (no data)"
+          
+        response = agent.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=#{s}&apikey=#{ENV['AA_KEY']}")
+        loop_start = Time.now
+        resp = JSON.parse(response.body)
+        if !resp["Information"].nil?
+          raise
+        end
+        
+        ohlc_hash = resp
+        
+        s_items = s_array.select { |sa| sa[0] == s }
+        
+        s_items.each do |si|
+          # write data to output spreadsheet
+          if si[2].nil?
+            # skip if problem pulling data
+            if !ohlc_hash["Information"].nil?
+              page.row(r).push si[0], si[1], "API called too fast", "API called too fast", "API called too fast", "API called too fast"
+            end
+            if ohlc_hash['Time Series (Daily)'][si[1]].nil?
+              page.row(r).push si[0], si[1], "N/A (no data)", "N/A (no data)", "N/A (no data)", "N/A (no data)"
+            else
+              page.row(r).push si[0], si[1], ohlc_hash['Time Series (Daily)'][si[1]]['1. open'], ohlc_hash['Time Series (Daily)'][si[1]]['2. high'], ohlc_hash['Time Series (Daily)'][si[1]]['3. low'], ohlc_hash['Time Series (Daily)'][si[1]]['4. close']
+            end
+          else
+            page.row(r).push si[0], si[1], si[2], si[3], si[4], si[5]
+          end
+          r += 1
+        end
+        
+        puts "Succeeded with #{s}"
+      rescue
+        if retries < 100
+          puts "Failed #{s}, try ##{retries + 1}"
+          retries += 1
+          sleep(4)
+          retry
         else
-          page.row(i+1).push s[0], s[1], ohlc_hash[s[0]]['Time Series (Daily)'][s[1]]['1. open'], ohlc_hash[s[0]]['Time Series (Daily)'][s[1]]['2. high'], ohlc_hash[s[0]]['Time Series (Daily)'][s[1]]['3. low'], ohlc_hash[s[0]]['Time Series (Daily)'][s[1]]['4. close']
+          next
         end
-      else
-        page.row(i+1).push s[0], s[1], s[2], s[3], s[4], s[5]
       end
     end
-    
+
     puts "Writing out and mailing output spreadsheet..."
     output.write ('temp_ohlc.xls')
     UpdateMailer.ohlc_email(email).deliver_now
